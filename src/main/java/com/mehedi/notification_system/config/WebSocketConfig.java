15185228e648be43fe956ae8b11ec3001c4ce919
Mehedi Hasan <mh.ju1803@gmail.com>
2024-12-13 12:55:45 +0600

    done

--- src/main/java/com/mehedi/notification_system/config/WebSocketConfig.java
+++ src/main/java/com/mehedi/notification_system/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/anyurl")
-                .setAllowedOrigins("*")
+                .setAllowedOrigins("http://localhost:63342")
                 .withSockJS();
     }
 }
